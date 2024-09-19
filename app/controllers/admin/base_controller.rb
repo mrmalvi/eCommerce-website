3b76611961a09be87eaec93aebe6c4b1d87da97c
Kamlesh Parmar <76098794+mrmalvi@users.noreply.github.com>
2024-09-19 16:35:27 +0530

    user status to access for authority (#2)

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,9 +1,18 @@
 module Admin
   class BaseController < ApplicationController
     before_action :authenticate_admin!
+    before_action :ensure_user_status!
 
     def authenticate_admin!
-      redirect_to root_path unless current_user&.admin?
+      redirect_to root_path unless user_signed_in? && current_user.admin?
+    end
+
+    def ensure_user_status!
+      if user_signed_in? && !current_user.active?
+        flash[:error] = "User has been #{current_user.status.capitalize}!!"
+        sign_out(current_user)
+        redirect_to root_path
+      end
     end
   end
 end
