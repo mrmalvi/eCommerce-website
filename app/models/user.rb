0667efd2cdd615efcda3d1f113ba7becb1ed87d7
mrmalvi <malviyak00@gmail.com>
2024-09-19 16:40:13 +0530

    user status to access for authority

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: { user: 'user', admin: 'admin' }
+  enum status: { active: 'active', inactive: 'inactive', banned: 'banned' }
 
   before_create :set_default_role
 
