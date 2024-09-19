21595e9026aa26da2a3587d474e72b9c6362cefd
Kamlesh Parmar <76098794+mrmalvi@users.noreply.github.com>
2024-09-19 16:58:42 +0530

    authority users (#3)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
   def after_sign_in_path_for(resource)
-    if resource.is_a?(User) && resource&.admin?
+    if user_signed_in? && !resource.active?
+      flash.clear
+      flash[:error] = "User has been #{resource.status.capitalize}!!"
+      sign_out(resource)
+      root_path
+    elsif resource.is_a?(User) && resource&.admin?
       stored_location_for(resource) || admin_products_path
     else
       root_path
